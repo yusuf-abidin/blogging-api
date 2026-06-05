e62c19d0840399288ad122ca47dd9af6aa9de34a
Yusuf-Abidin-Nurrahman <yusufabidin02@gmail.com>
2026-06-05 10:55:42 +0700

    Added verified status authority mapping in UserPrincipal

--- src/main/java/com/neratama/api/security/UserPrincipal.java
+++ src/main/java/com/neratama/api/security/UserPrincipal.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -18,7 +19,14 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+
+        if (user.isVerified()) {
+            authorities.add(new SimpleGrantedAuthority("VERIFIED"));
+        }
+
+        return authorities;
     }
 
     @Override
